52c775ed8705fe897e17e3a46b9b694a4e06e687
Hanan Hussain <hhussain141003@gmail.com>
2023-05-09 14:16:54 +0100

    Push

--- test1.py
+++ test1.py
@@ -3,56 +3,61 @@
 
 import csv
 from prettytable import from_csv
+from prettytable import PrettyTable
 
 # Display CSV Table at the start of the application.
-with open("Data.csv") as fp:
-    varObjTable = from_csv(fp)
-print(varObjTable)
+def table():
+    with open("Data.csv") as fp:
+        table = from_csv(fp)
+    print(table)
 
-
-def addRecord():
+# Add New record function.
+def add_record():
     with open('Data.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
 
-        # determine the next issue number by finding the highest issue number in the existing data
+        # Automatically determine the primary key (IssueNo + 1)
         with open('Data.csv', mode='r') as existing_file:
             reader = csv.DictReader(existing_file)
-            issue_numbers = [int(row['IssueNo.']) for row in reader]
-            next_issue_number = max(issue_numbers) + 1
+            IssueNo = [int(row['IssueNo']) for row in reader]
+            IssueNo = max(IssueNo) + 1
+        Name = input("Enter your name: ")
+        Surname = input("Enter your surname: ")
+        Telephone = input("Enter your telephone number: ")
+        Email = input("Enter your email: ")
+        Severity = input("Enter the severity (Low, Medium, or High): ")
+        Type = input("Enter the issue type: ")
+        Comments = input("Enter any comments: ")
+        Days_Affected = input("Enter the number of days affected: ")
+        Solved = input("Is the issue solved? (Yes or No): ")
 
-        # prompt the user for the new issue data
-        name = input("Enter your name: ")
-        surname = input("Enter your surname: ")
-        telephone = input("Enter your telephone number: ")
-        email = input("Enter your email: ")
-        severity = input("Enter the severity (Low, Medium, or High): ")
-        issue_type = input("Enter the issue type: ")
-        comments = input("Enter any comments: ")
-        days_affected = input("Enter the number of days affected: ")
-        solved = input("Is the issue solved? (Yes or No): ")
-
-        # write the new issue data to the CSV file
-        writer.writerow([next_issue_number, name, surname, telephone, email, severity, issue_type, comments, days_affected, solved])
+        file.seek(0, 2)  # move the pointer to the end of the file
+        writer.writerow([IssueNo, Name, Surname, Telephone, Email, Severity, Type, Comments, Days_Affected, Solved])
 
     print("Issue added successfully!")
-# Let the user decide whether they want to Create a new record, Edit an existing record or Delete an existing record.
-varUserAction = ""
 
-while varUserAction != "Exit":
-    varUserAction = input("Please type in the action you want to carry out (New/Edit/Delete/Exit): ")
+
+
+# Let the user decide whether they want to Create a new record, Edit an existing record or Delete an existing record.
+user_action = ""
+
+table()
+while user_action != "Exit":
+    user_action = input("Please type in the action you want to carry out (New/Edit/Delete/Exit): ")
 # ----------------------------------------------------------------------------------------------------------------------
-    if varUserAction == "New":
-        addRecord()
-        print("Test: New")
+    if user_action == "New":
+        add_record()
+        print("New Record added successfully!")
+        table()
 # ----------------------------------------------------------------------------------------------------------------------
-    elif varUserAction == "Edit":
+    elif user_action == "Edit":
         print("Test: Edit")
 # ----------------------------------------------------------------------------------------------------------------------
-    elif varUserAction == "Delete":
+    elif user_action == "Delete":
         print("Test: Delete")
 # ----------------------------------------------------------------------------------------------------------------------
-    elif varUserAction == "Exit":
-        print("Test: Exit")
+    elif user_action == "Exit":
+        print("Exited the app successfully")
 # ----------------------------------------------------------------------------------------------------------------------
     else:
         print("Error, please enter a correct value")
