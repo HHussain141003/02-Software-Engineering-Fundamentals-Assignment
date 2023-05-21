f4defefc4096ee628df1ac6290dbe74cda81ea22
Hanan Hussain <hhussain141003@gmail.com>
2023-05-21 10:39:36 +0100

    Push

--- test1.py
+++ test1.py
@@ -68,14 +68,15 @@ def add_record():
             else:
                 break
 
-        file.seek(0, 2)  # move the pointer to the end of the file
+        file.seek(0, 2)  # Move the pointer to the end of the file
         writer.writerow([IssueNo, Name, Surname, Telephone, Email, Severity, Type, Comments, Days_Affected, Solved])
 
     print("Issue added successfully!")
 
 # Delete record function
 def delete_record():
-    Issue_No = input("Enter the IssueNo to delete: ")
+    Issue_No = input("Enter the Issue Number you want to delete: ")
+    confirmation = ""
     temp_file = "temp.csv"
 
     with open('Data.csv', mode='r') as file, open(temp_file, mode='w', newline='') as temp:
@@ -90,7 +91,92 @@ def delete_record():
     os.remove('Data.csv')
     os.rename(temp_file, 'Data.csv')
     print("Record deleted successfully!")
+        
+# Edit record function
+def edit_record():
 
+    issue_no = input("Enter the issue number to edit: ")
+
+    with open('Data.csv', mode='r', newline='') as existing_file:
+        reader = csv.DictReader(existing_file)
+        rows = [row for row in reader]
+
+    # Find IssueNo Index
+    row_index = None
+    for i, row in enumerate(rows):
+        if row['IssueNo'] == issue_no:
+            row_index = i
+            break
+
+    # If the issue number was not found, print an error message and return
+    if row_index is None:
+        print(f"Issue number {issue_no} not found")
+        return
+
+    # Get the existing row values
+    existing_row = rows[row_index]
+
+    # Display the existing values and prompt for new values
+    print("Existing information:")
+    for key, value in existing_row.items():
+        print(f"{key}: {value}")
+
+    print("\nEnter new information (leave blank to keep existing value):")
+    name = input("Name: ")
+    if not name:
+        name = existing_row['Name']
+
+    surname = input("Surname: ")
+    if not surname:
+        surname = existing_row['Surname']
+
+    telephone = input("Telephone: ")
+    if not telephone:
+        telephone = existing_row['Telephone']
+
+    email = input("Email: ")
+    if not email:
+        email = existing_row['Email']
+
+    severity = input("Severity (Low, Medium, High): ")
+    if not severity:
+        severity = existing_row['Severity']
+
+    issue_type = input("Issue type: ")
+    if not issue_type:
+        issue_type = existing_row['Type']
+
+    comments = input("Comments: ")
+    if not comments:
+        comments = existing_row['Comments']
+
+    days_affected = input("Days affected: ")
+    if not days_affected:
+        days_affected = existing_row['Days_Affected']
+
+    solved = input("Solved? (Yes or No): ")
+    if not solved:
+        solved = existing_row['Solved']
+
+    # Update the row values
+    rows[row_index]['Name'] = name
+    rows[row_index]['Surname'] = surname
+    rows[row_index]['Telephone'] = telephone
+    rows[row_index]['Email'] = email
+    rows[row_index]['Severity'] = severity
+    rows[row_index]['Type'] = issue_type
+    rows[row_index]['Comments'] = comments
+    rows[row_index]['Days_Affected'] = days_affected
+    rows[row_index]['Solved'] = solved
+
+    # Write the updated rows back to the file
+    with open('Data.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['IssueNo', 'Name', 'Surname', 'Telephone', 'Email', 'Severity', 'Type', 'Comments', 'Days_Affected', 'Solved'])
+        for row in rows:
+            writer.writerow([row['IssueNo'], row['Name'], row['Surname'], row['Telephone'], row['Email'], row['Severity'], row['Type'], row['Comments'], row['Days_Affected'], row['Solved']])
+
+    print("Record edited successfully!")
 # Let the user decide whether they want to Create a new record, Edit an existing record or Delete an existing record.
 user_action = ""
 
@@ -110,7 +196,8 @@ while user_action != "X":
         table()
 # ----------------------------------------------------------------------------------------------------------------------
     elif user_action == "E":
-        print("Test: Edit")
+        edit_record()
+        table()
 # ----------------------------------------------------------------------------------------------------------------------
     elif user_action == "D":
         delete_record()
